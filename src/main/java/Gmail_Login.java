34edb88b0773db34006d19b46ae385def162a90b
10thirtyfour <neddle1034@gmail.com>
2015-11-10 17:22:30 +0200

    first commit

--- src/main/java/Gmail_Login.java
+++ src/main/java/Gmail_Login.java
@@ -26,64 +26,27 @@ public class Gmail_Login {
         // objects and variables instantiation
         logger.info("Running driver...");
         WebDriver driver = new FirefoxDriver();
-
         logger.info("Launch browser...");
-        // launch the firefox browser and open the application url
         driver.get(APP_URL);
-
         logger.info("Maximize browser window...");
-        // maximize the browser window
         driver.manage().window().maximize();
-        // declare and initialize the variable to store the expected title of the webpage.
-
         logger.info("Test has started...");
-        String expectedTitle = "Sign in - Google Accounts";
-        // fetch the title of the web page and save it into a string variable
+        String expectedTitle = "Sign in - Google Accounts  ";
         String actualTitle = driver.getTitle();
-
-
-
-        // TODO: так было
-//        if (expectedTitle.equals(actualTitle))
-//        {
-//            logger.info("Verification Successful - The correct title is displayed on the web page.");
-//        }
-//        else
-//        {
-//            logger.info("Verification Failed - An incorrect title is displayed on the web page.");
-//        }
-
-        //TODO: так надо
         Assert.assertEquals("Title is incorrect", expectedTitle, actualTitle);
-
-
-
-        // enter a valid username in the email textbox
         WebElement username = driver.findElement(By.id("Email"));
         username.clear();
         username.sendKeys("TestSelenium");
-        // enter a valid password in the password textbox
         WebElement NextButton = driver.findElement(By.id("next"));
         NextButton.click();
-
-        //yours implementation
-//        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-//        WebElement password = driver.findElement(By.id("Passwd"));
-
-        //alternative way
         WebElement password = (new WebDriverWait(driver, 30))
                 .until(ExpectedConditions.presenceOfElementLocated(By.id("Passwd")));
-
         password.clear();
-
         password.sendKeys("password123");
-      //   click on the Sign in button
         WebElement SignInButton = driver.findElement(By.id("signIn"));
         SignInButton.click();
-        // close the web browser
         driver.close();
         logger.info("Test script executed successfully.");
-        // terminate the program
         driver.quit();
     }
 }
