d37c10fe26e3673a35c1a4ffee6c2a4f673a88ed
shmublon <ana@3rhia>
2015-11-16 14:19:11 +0100

    small fixes

--- src/test/java/Character_Create.java
+++ src/test/java/Character_Create.java
@@ -18,19 +18,17 @@ public class Character_Create {
     protected static final Logger logger = Logger.getLogger(Character_Create.class);
 
     public static final String APP_URL = "http://character-gen.herokuapp.com/view/personage_manager.html";
-    String name = "TestName";
-    String age = "50";
-    String exp = "200";
+    private static final String NAME = "TestName";
+    private static final String AGE = "50";
+    private static final String EXP = "200";
 
-    String charLinkXpath = "//*[contains(text(),'"+name+"')]";
+    String charLinkXpath = "//*[contains(text(),'" + NAME + "')]";
 
-    public String findCharacterId (WebDriver driver, String name)
-    {
+    public String findCharacterId(WebDriver driver, String name) {
         WebElement characterLink = driver.findElement(By.xpath(charLinkXpath));
         String linkText = characterLink.getAttribute("href");
         String[] parts = linkText.split("=");
-        String characterLinkId = parts[1];
-        return (characterLinkId);
+        return parts[1];
     }
 
     @Test
@@ -38,7 +36,7 @@ public class Character_Create {
         // objects and variables instantiation
         logger.info("Running driver...");
         WebDriver driver = new FirefoxDriver();
-        CharacterCreationPage characterCreationPage = PageFactory.initElements(driver,CharacterCreationPage.class);
+        CharacterCreationPage characterCreationPage = PageFactory.initElements(driver, CharacterCreationPage.class);
         logger.info("Launch browser...");
         characterCreationPage.open(APP_URL);
         logger.info("Maximize browser window...");
@@ -49,17 +47,17 @@ public class Character_Create {
 
         try {
             characterCreationPage.characterCreateButton().click();
-            characterCreationPage.inputCharacterName(name);
-            characterCreationPage.inputCharacterAge(age);
-            characterCreationPage.inputCharacterExp(exp);
+            characterCreationPage.inputCharacterName(NAME);
+            characterCreationPage.inputCharacterAge(AGE);
+            characterCreationPage.inputCharacterExp(EXP);
             characterCreationPage.CharacterSubmitButton().click();
 
-            String deleteButtonLinkXpath = "//*[@name='id' and @value='"+
-                    findCharacterId(driver,name)+
+            String deleteButtonLinkXpath = "//*[@name='id' and @value='" +
+                    findCharacterId(driver, NAME) +
                     "']/following-sibling::a[@class='deletePersonage']";
             driver.navigate().refresh();
-            Assert.assertEquals ("Age doesn`t match", age, driver.findElement(By.xpath(charLinkXpath+"/parent::*/following-sibling::td[1]")).getText());
-            Assert.assertEquals ("Exp doesn`t match", exp, driver.findElement(By.xpath(charLinkXpath+"/parent::*/following-sibling::td[3]")).getText());
+            Assert.assertEquals("Age doesn`t match", AGE, driver.findElement(By.xpath(charLinkXpath + "/parent::*/following-sibling::td[1]")).getText());
+            Assert.assertEquals("Exp doesn`t match", EXP, driver.findElement(By.xpath(charLinkXpath + "/parent::*/following-sibling::td[3]")).getText());
             driver.findElement(By.xpath(deleteButtonLinkXpath)).click();
 
         } finally {
