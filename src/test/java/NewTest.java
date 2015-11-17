b58da4c5efb4334f592c89e3659c3f6be72c735d
shmublon <ana@3rhia>
2015-11-17 18:49:27 +0100

    example

---
+++ src/test/java/NewTest.java
@@ -0,0 +1,12 @@
+/**
+ * Created by artemk on 11/17/15.
+ */
+public class NewTest extends BaseTest {
+
+    @Override
+    public void setUp() throws InterruptedException {
+        //тут происходит тоже самое что и в базовом сетапе, тоесть общее для всех, запуск браузера и всякая такая ебола
+        super.setUp();
+        //тут происходить любая хуета для сетапа конкретно этого теста
+    }
+}
